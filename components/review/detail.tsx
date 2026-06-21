c3cc7baaa57d2f1d80e485952154500052c96758
acemaiyeu <nthanhhuy82@gmail.com>
2026-06-21 22:03:46 +0700

    router param

--- components/review/detail.tsx
+++ components/review/detail.tsx
@@ -1,18 +1,26 @@
 import { Button, StyleSheet, Text, View } from "react-native"
 import { OPENSAN_REGULAR } from "../../utils/const";
-import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { NavigationProp, RouteProp, useNavigation, useRoute } from "@react-navigation/native";
 
 const styles = StyleSheet.create({
     review: {
         fontSize: 30,
         fontFamily: OPENSAN_REGULAR
-    }
+    },
+    reviewText: {
+        fontSize: 25,
+        fontFamily: OPENSAN_REGULAR,
+        padding: 15
+    },
 })
 const DetailScreen = () => {
     const navigation: NavigationProp<RootStackParamList> = useNavigation();
+    const route: RouteProp<RootStackParamList, 'review-detail'> = useRoute();
     return (
         <View>
-            <Text style={styles.review}>Detail google</Text>
+            <Text style={styles.reviewText}>ID: {route.params?.id}</Text>
+            <Text style={styles.reviewText}>Tiêu đề: {route.params?.title}</Text>
+            <Text style={styles.reviewText}>Rating: {route.params?.star}</Text>
             <Button title="Go home" onPress={() => navigation.navigate("home")}/>
         </View>
     )
--- components/review/home.tsx
+++ components/review/home.tsx
@@ -1,12 +1,42 @@
-import { Button, Text, View } from "react-native"
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import { Button, FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native"
 
-const HomeScreen = (props: any) => {
-    const {navigation} = props;
+
+interface IRVIEW {
+    id: number,
+    title: string,
+    star: number
+}
+const styles = StyleSheet.create({
+    reviewItem: {
+        backgroundColor: "aqua",
+        margin: 15, 
+        padding: 15
+    }
+})
+const HomeScreen = () => {
+    const navigation: NavigationProp<RootStackParamList> = useNavigation();
+    const [reviews, setReviews] = useState<IRVIEW[]>([
+        {id: 1, title: "react-native", star: 5},
+        {id: 2, title: "almo", star: 5},
+    ]);
     return (
         <View>
-            <Text>Home</Text>
-            <View>
+            <Text>Danh sách chương trình: </Text>
+            {/* <View>
                 <Button title="VIew detail" onPress={() => navigation.navigate("review-detail")}/>
+            </View> */}
+            <View>
+                    <FlatList data={reviews}  keyExtractor={item => item.id +""} renderItem={({item}) => {
+                        return (
+                            <TouchableOpacity onPress={() => navigation.navigate("review-detail", item)}>
+                                <View style={styles.reviewItem}>
+                                    <Text>{item.title}</Text>
+                                </View>
+                            </TouchableOpacity>
+                        )
+                    }} />
             </View>
         </View>
     )
